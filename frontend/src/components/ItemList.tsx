22cc5740ea7e52eaf42d8aa95b04bae6f6ab999a
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2020-02-28 17:37:32 -0700

    enable switch on demo

--- frontend/src/components/ItemList.tsx
+++ frontend/src/components/ItemList.tsx
@@ -317,24 +317,25 @@ const ItemList: React.FC<ItemListProps> = props => {
             </IconButton>
           </Tooltip>
           <SharePopover isMobile={isMobile} />
-          {isListOwner && (
-            <Tooltip title={editMode ? 'Edit mode' : 'View mode'}>
-              <Switch
-                color="secondary"
-                size={isMobile ? 'medium' : 'small'}
-                checked={editMode}
-                onChange={() =>
-                  dispatch(
-                    actions.setEditMode({
-                      editMode: !editMode,
-                      listId: list.id,
-                    })
-                  )
-                }
-                value="edit"
-              />
-            </Tooltip>
-          )}
+          {isListOwner ||
+            (list.id === 'demo' && (
+              <Tooltip title={editMode ? 'Edit mode' : 'View mode'}>
+                <Switch
+                  color="secondary"
+                  size={isMobile ? 'medium' : 'small'}
+                  checked={editMode}
+                  onChange={() =>
+                    dispatch(
+                      actions.setEditMode({
+                        editMode: !editMode,
+                        listId: list.id,
+                      })
+                    )
+                  }
+                  value="edit"
+                />
+              </Tooltip>
+            ))}
           <Button
             color="secondary"
             size={isMobile ? 'large' : 'medium'}
